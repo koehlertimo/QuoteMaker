b8652ca65b502a830b0eaed5e82584a7a2d5800e
Timo Köhler <hello@timokoehler.com>
2019-04-24 21:07:35 +0200

    created a request method

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Hello World")
+#imports
+import requests
+
+
+#GET a Image from Unsplash API
+r = requests.get("https://static4.businessinsider.de/image/5a8d786722e6c638008b491f-2400/shutterstock586550951.jpg")
+
+with open('test.png', 'wb') as f:
+    f.write(r.content)
+
+
+#transform it with a black bg
+
+#Resize
+
+#Logo etc.
+
+#GET a random quote
+
+#put Quote on the picture
+
+#save final pictutre
