118127f04ba5de49e521d08b2ec846d34b89cca2
Timo Köhler <hello@timokoehler.com>
2019-04-25 21:20:12 +0200

    fixed a note

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 
 ###Generate a Color###
 
-#a list of random numbers
+#a list of rgb color codes
 colors = [(26, 188, 156),(46, 204, 113),(39, 174, 96),(22, 160, 133),(52, 152, 219),(41, 128, 185),(155, 89, 182),(142, 68, 173),(52, 73, 94),(44, 62, 80),(230, 126, 34),(211, 84, 0),(231, 76, 60),(192, 57, 43)]
 
 img = Image.new('RGB', (800, 800), random.choice(colors)) #choose a random number out of the list
