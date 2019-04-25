26a85f0a39fa3b25943c43aa0db17ad5b436ffc2
Timo Köhler <hello@timokoehler.com>
2019-04-25 21:19:08 +0200

    random color picker!

--- main.py
+++ main.py
@@ -2,12 +2,13 @@
 from PIL import Image
 import random
 
-#Generate a Color
+###Generate a Color###
 
+#a list of random numbers
 colors = [(26, 188, 156),(46, 204, 113),(39, 174, 96),(22, 160, 133),(52, 152, 219),(41, 128, 185),(155, 89, 182),(142, 68, 173),(52, 73, 94),(44, 62, 80),(230, 126, 34),(211, 84, 0),(231, 76, 60),(192, 57, 43)]
 
-img = Image.new('RGB', (800, 800), random.choice(colors))
-img.save("imagetest.jpg")
+img = Image.new('RGB', (800, 800), random.choice(colors)) #choose a random number out of the list
+
 
 #Logo etc.
 
