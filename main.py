100d72cc51408eae9d05b9a6e028ad2612881aff
Timo Köhler <hello@timokoehler.com>
2019-04-25 20:43:20 +0200

    added a list of random numbers

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 #imports
 from PIL import Image
+import random
 
 #Generate a Color
 
-img = Image.new('RGB', [800, 800], color ='red')
-img.save("imagetest.jpg")
+colors = [(26, 188, 156),(46, 204, 113),(39, 174, 96),(22, 160, 133),(52, 152, 219),(41, 128, 185),(155, 89, 182),(142, 68, 173),(52, 73, 94),(44, 62, 80),(230, 126, 34),(211, 84, 0),(231, 76, 60),(192, 57, 43)]
 
-#Resize
+img = Image.new('RGB', (800, 800), random.choice(colors))
+img.save("imagetest.jpg")
 
 #Logo etc.
 
