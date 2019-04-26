634a00afafdc36aed358017da10a5435ab615626
Timo Köhler <hello@timokoehler.com>
2019-04-26 14:45:41 +0200

    Updated README, added logo.png, tested an image pasting method

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Quote Maker
 > Simple script that creates Quote Images
 
-This script get a quote from an csv file and put this text on an random Unspleash picture and save this as an png.
+This script get a quote from an csv file and put this text on an random color background and save this as an png.
 
 
 ## Installation
--- main.py
+++ main.py
@@ -7,10 +7,16 @@ import random
 #a list of rgb color codes
 colors = [(26, 188, 156),(46, 204, 113),(39, 174, 96),(22, 160, 133),(52, 152, 219),(41, 128, 185),(155, 89, 182),(142, 68, 173),(52, 73, 94),(44, 62, 80),(230, 126, 34),(211, 84, 0),(231, 76, 60),(192, 57, 43)]
 
-img = Image.new('RGB', (800, 800), random.choice(colors)) #choose a random number out of the list
+bg = Image.new('RGB', (800, 800), random.choice(colors)) #choose a random number out of the list
 
 
-#Logo etc.
+#add Logo
+
+logo = Image.open("logo.png")
+
+imgbg = bg.paste(logo, (0,0,0,0))
+
+imgbg.save("./img/test.png")
 
 #GET a random quote
 
